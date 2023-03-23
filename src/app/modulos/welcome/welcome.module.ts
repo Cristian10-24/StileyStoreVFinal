7bc32ed6c96d45820d7c4cd0b075bd1399de37df
manco <mejialopez.lmj@gamil.com>
2023-03-22 20:42:59 -0500

    actualizacion del welcome(ofertas)

--- src/app/modulos/welcome/welcome.module.ts
+++ src/app/modulos/welcome/welcome.module.ts
@@ -8,6 +8,11 @@ import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 import { PrimeraSesionComponent } from './componentes/primera-sesion/primera-sesion.component';
 import { HomeSecundarioComponent } from './pages/home-secundario/home-secundario.component';
 import { HeaderSecundarioComponent } from './componentes/header-secundario/header-secundario.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { Carrusel2Component } from './pages/ofertas/carrusel2/carrusel2.component';
+import { Seccion1Component } from './pages/ofertas/seccion1/seccion1.component';
+import { Seccion2Component } from './pages/ofertas/seccion2/seccion2.component';
+import { Seccion3Component } from './pages/ofertas/seccion3/seccion3.component';
 
 
 @NgModule({
@@ -16,7 +21,12 @@ import { HeaderSecundarioComponent } from './componentes/header-secundario/heade
     CarruselComponent,
     PrimeraSesionComponent,
     HomeSecundarioComponent,
-    HeaderSecundarioComponent
+    HeaderSecundarioComponent,
+    OfertasComponent,
+    Carrusel2Component,
+    Seccion1Component,
+    Seccion2Component,
+    Seccion3Component
   ],
   imports: [
     CommonModule,
